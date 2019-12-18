794ea12625dacb009cd4205ddc687cc06cbbc143
SeakmengChheang <c.seakmeng0603@gmail.com>
2019-12-18 14:34:02 +0700

    'organize'

---
+++ course_handler.php
@@ -0,0 +1,30 @@
+<?php session_start(); ?>
+
+<div>
+    <div class="button-bar">
+        <button><?php $_SESSION['role'] == 'student'
+                    ? print 'My Course'
+                    : print 'Added Courses' ?></button>
+        <button>View All</button>
+    </div>
+
+    <table>
+        <thead>
+            <th>Academic</th>
+            <th>Semester</th>
+            <th>Course Name</th>
+            <th>Course Code</th>
+            <th>Course Group</th>
+            <th>Course Description</th>
+            <?php
+                if($_SESSION['role'] == 'student')
+                    echo "<th>Author</th>";
+            ?>
+            
+        </thead>
+
+        <tbody>
+
+        </tbody>
+    </table>
+</div>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+    <div class="navbar">
+        <img src="images/logo.png" alt="piu-logo" height="75px" width="250px">
+
+        <div class="menu">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Course</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Profile</a></li>
+                <li><a href="">Log out</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="body">
+        <?php include 'course_handler.php'; ?>
+    </div>
+
+    <div class="footer">
+        <p>Copyright &copy; Paragon IU 2019</p>
+    </div>
+</body>
+
+</html>
